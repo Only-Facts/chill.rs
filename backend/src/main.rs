8a8636d7e3850210e842454eebfee45f6af89064
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-06-25 07:25:43 +0200

    [UPDATE] main.rs
    [ADD] music
    [ADD] app_state
    [ADD] music_dir
    [ADD] music services

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,9 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+mod music;
+
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+use std::{path::PathBuf, sync::Mutex};
+
+const MUSIC_DIRECTORY_ENV_VAR: &str = "MUSIC_DIR";
 
 #[get("/")]
 async fn default() -> impl Responder {
@@ -6,9 +11,32 @@ async fn default() -> impl Responder {
 }
 
 #[actix_web::main]
+#[allow(clippy::manual_strip, clippy::io_other_error)]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(default))
-        .bind(("127.0.0.1", 8081))?
-        .run()
-        .await
+    let music_dir_str =
+        std::env::var(MUSIC_DIRECTORY_ENV_VAR).unwrap_or_else(|_| String::from("./music"));
+    let music_dir = PathBuf::from(music_dir_str);
+    let tracks_map = match music::load_music_files(&music_dir).await {
+        Ok(map) => map,
+        Err(e) => {
+            eprintln!("Error loading music files: {}", e);
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, e));
+        }
+    };
+    let app_state = web::Data::new(music::AppState::new(
+        music_dir.clone(),
+        Mutex::new(tracks_map),
+    ));
+
+    HttpServer::new(move || {
+        App::new().service(
+            web::scope("/music")
+                .app_data(app_state.clone())
+                .service(music::list)
+                .service(music::stream),
+        )
+    })
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
 }
