a490c450c506a166317ac0c7aa21305ecc104d70
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-06-27 01:23:01 +0200

    [UPDATE] auth.rs
    [ADD] show_user()

--- backend/src/api/auth.rs
+++ backend/src/api/auth.rs
@@ -13,3 +13,14 @@ pub async fn show_users(pool: web::Data<MySqlPool>) -> impl Responder {
     }
 }
 
+#[get("/user/{id}")]
+pub async fn show_user(pool: web::Data<MySqlPool>, id: web::Path<u32>) -> impl Responder {
+    let wid = id.into_inner();
+    match db::get_user(pool.get_ref(), wid).await {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(err) => {
+            eprintln!("Failed to fetch users: {}", err);
+            HttpResponse::InternalServerError().body("Error fetching users")
+        }
+    }
+}
